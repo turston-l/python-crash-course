378ac01e2eab6409f8badc98136fe31f5b797e56
Turston <turstonds@gmail.com>
2021-11-08 15:15:29 +0200

    string exercises

---
+++ basics/name_cases.py
@@ -0,0 +1,18 @@
+first_name = "Eric"
+print(f"Hello {first_name}, would you like to learn some Python today?")
+
+second_name = "Billy Bob"
+print(second_name.lower())
+print(second_name.upper())
+print(second_name.title())
+
+quote = "If you can see it in your mind, you can hold it in your hand"
+person = "Bob Proctor"
+
+famous_quote = f'{person} once said, "{quote}".'
+print(famous_quote)
+
+third_name = "\t Al Passio \n"
+print(third_name.lstrip())
+print(third_name.rstrip())
+print(third_name.strip())
