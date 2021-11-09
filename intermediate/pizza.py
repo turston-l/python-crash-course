03e2da4a470f05f4669acf76bd1b9e83d15b7bb8
Turston <turstonds@gmail.com>
2021-11-09 16:00:26 +0200

    pizza exercise

---
+++ intermediate/pizza.py
@@ -0,0 +1,10 @@
+pizza_toppings = ['cheese', 'peppers', 'garlic']
+for pizza_topping in pizza_toppings:
+    print(pizza_topping)
+
+pizza_toppings = ['cheese', 'peppers', 'garlic']
+for pizza_topping in pizza_toppings:
+    print(f'I like {pizza_topping.title()} on my pizza.')
+
+print('I like pizza a.....\n')
+print('........whole lot!')
