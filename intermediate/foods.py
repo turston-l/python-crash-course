8d6c687d7b382c057b7c34d5766ad5fa26848309
Turston <turstonds@gmail.com>
2021-11-09 19:14:08 +0200

    data storage structures

---
+++ intermediate/foods.py
@@ -0,0 +1,12 @@
+my_foods = ['pizza', 'lentils', 'fries']
+print(my_foods)
+
+friends_foods = my_foods[:]
+print(friends_foods)
+
+my_foods.append('patties')
+friends_foods.append('pasta')
+
+print(my_foods)
+print('\n')
+print(friends_foods)
