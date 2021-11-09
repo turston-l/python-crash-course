d66daad44aa564f08478c4e9164c7bba82d07433
Turston <turstonds@gmail.com>
2021-11-09 06:30:44 +0200

    list introduction

---
+++ intermediate/bycycles.py
@@ -0,0 +1,21 @@
+bycycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bycycles)
+
+print('\n')
+
+print(bycycles[0])
+print(bycycles[0].title())
+
+print('\n')
+
+print(bycycles[1])
+print(bycycles[3])
+
+print('\n')
+
+print(bycycles[-1])
+
+print('\n')
+
+message = f'My first bycycle was a {bycycles[0].title()}.'
+print(message)
