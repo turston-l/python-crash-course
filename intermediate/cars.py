61d8e5b0d84c920a42d8ae0377229bdd056291b7
Turston <turstonds@gmail.com>
2021-11-09 13:30:45 +0200

    sorting lists

---
+++ intermediate/cars.py
@@ -0,0 +1,20 @@
+cars = ['toyota', 'audi', 'bmw', 'volvo']
+# cars.sort()
+# print(cars)
+
+# cars.sort(reverse=True)
+# print(cars)
+
+print("Here is the original list")
+print(cars)
+
+print("\nHere is the sorted list")
+print(sorted(cars))
+
+print("\nHere is the original list again")
+print(cars)
+
+cars.reverse()
+print(cars)
+
+print(len(cars))
