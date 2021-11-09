9460ace6f9d951d29ee81652a71b4f0aba31c87d
Turston <turstonds@gmail.com>
2021-11-09 12:50:29 +0200

    more list exercises

---
+++ intermediate/dinner_list.py
@@ -0,0 +1,44 @@
+guests = ['wallace wattles', 'bob proctor', 'steve pavlina']
+invitation_1 = f'{guests[0].title()}, I would like to invite you to dinner.'
+invitation_2 = f'{guests[1].title()}, I would like to invite you to dinner.'
+invitation_3 = f'{guests[2].title()}, I would like to invite you to dinner.'
+print(invitation_1)
+print(invitation_2)
+print(invitation_3)
+print(f"Unfortunately, {guests[0].title()} won't be able to make it.")
+removed_guest = guests.pop(0)
+guests.insert(0, 'david icke')
+print(removed_guest.title())
+print(invitation_1)
+print(invitation_2)
+print(invitation_3)
+print(guests)
+print(invitation_1)
+print(invitation_2)
+print(invitation_3)
+
+invitation_1 = f'{guests[0].title()}, I would like to invite you to dinner.'
+invitation_2 = f'{guests[1].title()}, I would like to invite you to dinner.'
+invitation_3 = f'{guests[2].title()}, I would like to invite you to dinner.'
+print(invitation_1)
+print(invitation_2)
+print(invitation_3)
+
+print("We've found a bigger dinner table.")
+
+guests.insert(0, 'jordan peterson')
+guests.insert(2, 'james clear')
+guests.append('val de vall')
+
+invitation_1 = f'{guests[0].title()}, I would like to invite you to dinner.'
+invitation_2 = f'{guests[1].title()}, I would like to invite you to dinner.'
+invitation_3 = f'{guests[2].title()}, I would like to invite you to dinner.'
+invitation_4 = f'{guests[3].title()}, I would like to invite you to dinner.'
+invitation_5 = f'{guests[4].title()}, I would like to invite you to dinner.'
+invitation_6 = f'{guests[5].title()}, I would like to invite you to dinner.'
+print(invitation_1)
+print(invitation_2)
+print(invitation_3)
+print(invitation_4)
+print(invitation_6)
+
