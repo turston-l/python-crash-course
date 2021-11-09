909864b889ff0d023be5d569a50c3f899985d682
Turston <turstonds@gmail.com>
2021-11-09 06:45:01 +0200

    list exercises

---
+++ intermediate/list_exercise.py
@@ -0,0 +1,15 @@
+names = ['Dan', 'Alberto', 'Kim']
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+
+print('\n')
+
+print(f'Hello {names[0].title()}, I miss you.')
+print(f'Hello {names[1].title()}, I miss you.')
+print(f'Hello {names[2].title()}, I miss you.')
+
+print('\n')
+
+cars = ['Mercedes', 'GTI', 'BMW']
+print(f'I would like to own a {cars[2]}.')
