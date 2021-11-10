76c604c41ddd5a10ff014fd4aa099cec6ed8d274
Turston <turstonds@gmail.com>
2021-11-10 17:45:01 +0200

    working with dictionaries

---
+++ intermediate/favorite_languages.py
@@ -0,0 +1,9 @@
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+
+sarahs_favorite = favorite_languages['sarah'].title()
+print(f'Sarahs favorite language is: {sarahs_favorite}')
