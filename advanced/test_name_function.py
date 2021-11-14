46bac032baea04f681b0cb7efa3149e1e8cb4936
Turston <turstonds@gmail.com>
2021-11-14 18:47:27 +0200

    testing the name function

---
+++ advanced/test_name_function.py
@@ -0,0 +1,12 @@
+from types import FunctionType
+import unittest
+FunctionType.py in name_function import get_formatted_name
+    
+class NamesTestCase(unittest.TestCase):
+    """Tests for 'name_function.py'."""
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+        if __name__ == '__main__':
+            unittest.main()
